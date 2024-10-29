bfa502cbe061a59c8073cf59eb16e6b79ba9abb4
Tiago Rafael <tiagorafaelmelo@gmail.com>
2024-10-28 22:03:39 -0300

    Atualizacoes

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    private $app_data;
+
+    public function __construct()
+    {
+        // load app_data.php file from folder
+        $this->app_data = require(app_path('app_data.php'));
+    }
+
+    public function showData()
+    {
+        return response()->json($this->app_data);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     echo "Hello World!";
 });
+
+Route::get('/show_data', [\App\Http\Controllers\MainController::class, 'showData']);
