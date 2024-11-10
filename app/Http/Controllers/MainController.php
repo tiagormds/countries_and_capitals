b4673c69305f1d63153a905e7b6f13c1562da246
Tiago Rafael <tiagorafaelmelo@gmail.com>
2024-11-10 17:32:23 -0300

    Atualizacoes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,7 +25,7 @@ class MainController extends Controller
         //validate request
         $request->validate([
             'total_questions' => 'required|integer|min:3|max:30',
-        ],[
+        ], [
             'total_questions.required' => 'O número de questões é obrigatório',
             'total_questions.integer' => 'O número de questões tem que ser valor inteiro',
             'total_questions.min' => 'No mínimo :min questões',
@@ -38,11 +38,70 @@ class MainController extends Controller
         //prepare all the quiz structure
         $quiz = $this->prepareQuiz($total_questions);
 
-        dd($quiz);
+        // store the quiz in session
+        session()->put([
+            'quiz' => $quiz,
+            'total_questions' => $total_questions,
+            'current_question' => 1,
+            'correct_answers' => 0,
+            'wrong_answers' => 0,
+        ]);
+
+        return redirect()->route('game');
     }
 
-    private function prepareQuiz($total_questions)
+    private function prepareQuiz($total_questions): array
     {
+        $questions = [];
+        $total_countries = count($this->app_data);
 
+        //create countries index for unique questions
+        $indexes = range(0, $total_countries - 1);
+        shuffle($indexes);
+        $indexes = array_slice($indexes, 0, $total_questions);
+
+        //create array of questions
+        $question_number = 1;
+        foreach ($indexes as $index) {
+            $question['question_number'] = $question_number++;
+            $question['country'] = $this->app_data[$index]['country'];
+            $question['correct_answer'] = $this->app_data[$index]['capital'];
+
+            // wrong answers
+            $other_capitals = array_column($this->app_data, 'capital');
+
+            // remove correct answer
+            $other_capitals = array_diff($other_capitals, [$question['correct_answer']]);
+
+            // shuffle the wrong answer
+            shuffle($other_capitals);
+            $question['wrong_answer'] = array_slice($other_capitals, 0, 3);
+
+            // store anwer result
+            $question['correct'] = null;
+            $questions[] = $question;
+        }
+
+        return $questions;
+    }
+
+    public function game(): View
+    {
+        $quiz = session('quiz');
+        $total_questions = session('total_questions');
+        $current_question = session('current_question') - 1;
+
+        // prepare answers to show in view
+        $asnwers = $quiz['$current_question'];
+        $asnwers[] = $quiz['$correct_answers'];
+
+        shuffle($asnwers);
+
+        return view('game')->with([
+            'country' => $quiz['$current_question']['country'],
+            'totalQuestions' => $total_questions,
+            'current_question' => $current_question,
+            'answers' => $asnwers,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 //start game
 Route::get('/', [\App\Http\Controllers\MainController::class, 'startGame'])->name('startGame');
 Route::post('/', [\App\Http\Controllers\MainController::class, 'prepareGame'])->name('prepareGame');
+
+//In game
+Route::get('/game', [\App\Http\Controllers\MainController::class, 'game'])->name('game');
