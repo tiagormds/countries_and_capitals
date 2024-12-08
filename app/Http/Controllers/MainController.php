e0771763f16d008acdeee3edb37268dda83cca9d
Tiago Rafael <tiagorafaelmelo@gmail.com>
2024-12-08 09:29:30 -0300

    Atualizacoes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -164,7 +164,14 @@ class MainController extends Controller
 
     public function showResults()
     {
-        echo 'Mostra resultados finais';
-        dd(session()->all());
+        $total_questions = session('total_questions');
+
+        return view('final_results')->with([
+            'correct_answers' => session('correct_answers'),
+            'wrong_answers' => session('wrong_answers'),
+            'total_questions' => session('total_questions'),
+            'percentage' => round((session('correct_answers') / session('total_questions')) * 100, 2),
+        ]);
+
     }
 }
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -17,7 +17,7 @@
 
     <!-- cancel game -->
     <div class="text-center mt-5">
-        <a href="#" class="btn btn-outline-danger mt-3 px-5">CANCELAR JOGO</a>
+        <a href="{{ route('startGame') }}" class="btn btn-outline-danger mt-3 px-5">CANCELAR JOGO</a>
     </div>
 
 </x-main-layout>
